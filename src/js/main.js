6f93606428edb17bc1d5b497deef2bd482372386
RomanPolozhii <roman.polozhii@gmail.com>
2020-05-23 15:35:43 +0300

    added clients section and adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 $(function () {
-  //IBG=====================================================
+  //IBG=======================================================
   function ibg() {
     $.each($(".ibg"), function (index, val) {
       if ($(this).find("img").length > 0) {
@@ -12,10 +12,38 @@ $(function () {
   }
 
   ibg();
-  //IBG=====================================================
+  //IBG========================================================
+  //Burger=====================================================
   $(".header__burger").click(function () {
     $(".header__burger").toggleClass("active"),
-    $(".header__list ").toggleClass("active"),
-    $("body").toggleClass("lock")
+      $(".header__list ").toggleClass("active"),
+      $("body").toggleClass("lock");
+  });
+  //Slider=====================================================
+  $(".clients__slider").slick({
+    slidesToShow: 4,
+    //slidesToScroll: 1,
+    //centerMode: true,
+    //arrows:true,
+    responsive: [
+      {
+        breakpoint: 1130,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 });
